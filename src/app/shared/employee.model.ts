ffc5d0ce27d4d21133facdfb7b171715c4f3cfa0
Bozhidar Rangelov <bojidar.rangelov@gmail.com>
2019-02-16 10:14:36 +0200

    Initial commit

---
+++ src/app/shared/employee.model.ts
@@ -0,0 +1,16 @@
+export class Employee {
+    EmployeeID: number;
+    LastName: string;
+    FirstName: string;
+    Title: string;
+    TitleOfCourtesy: string;
+    Address: string;
+    City: string;
+    Region: string;
+    PostalCode: string;
+    Country: string;
+    HomePhone: string;
+    Extension: string;
+    Notes: string;
+    PhotoPath: string;
+}
